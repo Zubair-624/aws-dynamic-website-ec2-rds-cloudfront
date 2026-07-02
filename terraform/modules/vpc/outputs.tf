f71a4a4c146a4b06aef87de0c55ebccdffb46fb1
Zubair624 <zubairmazumder624@gmail.com>
2026-07-02 23:15:11 +0600

    fix module vpc resources and fetech all the variable point to each-other

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,31 +1,27 @@
-#--------------------VPC ID Output--------------------
+#---VPC ID Output---
 output "output_aws_vpc_id" {
 
-    description = "ID of the vpc"
     value = aws_vpc.vpc.id
   
 }
 
-#----------Public Subnet IDs Output----------
+#---Public Subnet IDs Output---
 output "output_aws_vpc_public_subnet_ids" {
 
-    description = "ID of the public subnet"
     value = aws_subnet.aws_vpc_public_subnets[*].id
   
 }
 
-#----------Private Subnet IDs Output----------
+#---Private Subnet IDs Output---
 output "output_aws_vpc_private_subnet_ids" {
 
-    description = "ID of the private subnet"
     value = aws_subnet.aws_vpc_private_subnets[*].id 
   
 }
 
-#----------Public Route Table Output----------
+#---Public Route Table Output---
 output "output_aws_vpc_public_route_table_id" {
 
-    description = "ID of the public route table"
     value = aws_route_table.aws_vpc_public_rt.id 
   
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,30 +1,30 @@
-#----------Project Name----------
+#---Project Name---
 variable "project_name" {
-    description = "Project name used in the vpc all resources"
+    
     type = string
 }
 
-#----------VPC CIDR Block----------
+#---VPC CIDR Block---
 variable "aws_vpc_cidr_block" {
-    description = "CIDR Block for the aws vpc"
+
     type = string
     default = "10.0.0.0/16"
 }
 
-#----------Availability Zone----------
+#---Availability Zone---
 variable "azs" {
-    description = "AZ for public and private subnets"
+    
     type = list(string)
 }
 
-#----------Public Subnet CIDR----------
+#---Public Subnet CIDR---
 variable "aws_vpc_public_subnet_cidrs" {
-    description = "CIDR Block for the public subnet"
+    
     type = list(string)
 }
 
-#----------Private Subnet CIDR----------
+#---Private Subnet CIDR---
 variable "aws_vpc_private_subnet_cidrs" {
-    description = "CIDR Block for the private subnet"
+    
     type = list(string)
 }
